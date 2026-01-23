15b5620ecf318eb005b43a0c1849bdd4d973ec56
Michael Dawson-Haggerty <mikedh@kerfed.com>
2026-01-23 16:39:06 -0500

    config

---
+++ src/config.h
@@ -0,0 +1,9 @@
+// Minimal config.h for building CairoOutputDev outside of poppler's build system
+#ifndef POPPLER_CONFIG_H
+#define POPPLER_CONFIG_H
+
+#define PACKAGE "poppler"
+#define PACKAGE_VERSION "24.0.0"
+#define VERSION PACKAGE_VERSION
+
+#endif
