926dd9c5be09a8dc92820b58835ed6adc9e9f1ee
Michael Dawson-Haggerty <mikedh@kerfed.com>
2026-01-23 14:51:43 -0500

    try removing cmake special paths

--- src/pdf_to_svg/__init__.py
+++ src/pdf_to_svg/__init__.py
@@ -1,4 +1,13 @@
+import os
+import sys
 from typing import Literal
+
+_libs = os.path.join(os.path.dirname(__file__), '.libs')
+if os.path.exists(_libs):
+    if sys.platform == 'darwin':
+        os.environ['DYLD_LIBRARY_PATH'] = _libs + ':' + os.environ.get('DYLD_LIBRARY_PATH', '')
+    elif sys.platform == 'win32':
+        os.add_dll_directory(_libs)
 
 from ._core import pdf_to_svg
 
